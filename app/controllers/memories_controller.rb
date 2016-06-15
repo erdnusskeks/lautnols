e5f5af09f26b830f7b9de8b2ce6b719b44f749a3
erdnusskeks <erdnusskeks@googlemail.com>
2016-06-15 21:38:44 +0200

    Adding param subtitle

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -69,6 +69,6 @@ class MemoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def memory_params
-      params.require(:memory).permit(:name, :description, :picture)
+      params.require(:memory).permit(:name, :subtitle, :description, :picture)
     end
 end
